1ff9d7761668142988b468e933860579eec6c070
noohamol-km <noohamol770@gmail.com>
2025-12-03 11:07:18 +0530

    Delete co4/co5 directory

--- co4/co5/pg1.py
+++
@@ -1,5 +0,0 @@
-list1 = []
-with open('textfile.txt', 'r') as file1:
-    for line1 in file1:
-        list1.append(line1.split())
-print (list1)
--- co4/co5/pg3.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('student.csv', 'r')as file1:
-    reader=csv.reader(file1)
-    for row in reader:
-        print(row)
