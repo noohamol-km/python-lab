0c04c9eb64bedbfa054e2073a2af022fa49ef85f
noohamol-km <noohamol770@gmail.com>
2025-12-03 10:42:41 +0530

    Add files via upload

---
+++ co4/c04-1.py
@@ -0,0 +1,27 @@
+class Rect:
+   def __init__ (self, length, breadth): # Constructor to initialize length and breadth
+     self.l = length
+     self.b = breadth
+   def area(self): # Method to calculate area
+     return self.l * self.b
+   def perimeter(self): # Method to calculate perimeter
+     return 2 * (self.l + self.b)
+#Main program
+x1 = int(input("Enter length of 1st rectangle: "))
+y1 = int(input("Enter breadth of 1st rectangle: "))
+r1 = Rect(x1, y1)
+
+print("Area of rectangle 1:", r1.area())
+print("Perimeter of rectangle 1:", r1.perimeter())
+
+x2 = int(input("\nEnter length of 2nd rectangle: "))
+y2 = int(input("Enter breadth of 2nd rectangle: "))
+r2 = Rect(x2, y2)
+
+print("Area of rectangle 2:", r2.area())
+print("Perimeter of rectangle 2:", r2.perimeter())
+
+if r1.area() == r2.area(): #Compare the areas of the two rectangles
+    print("\nAreas of both rectangles are equal.")
+else:
+    print("\nAreas of both rectangles are not equal.")
